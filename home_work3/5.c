49a07edda371a7af9c0310281724815dc3145177
mvajhi <mvajhimv@gmail.com>
2022-11-13 23:09:00 +0330

    write hw3/5,6

---
+++ home_work3/5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+	int input = 0;
+	scanf("%i", &input);
+
+	long long int number_past1 = 1;
+	long long int number_past2 = 1;
+	long long int number = 0;
+
+	for (int i = 3; i <= input; i++)
+	{
+		number = number_past1 + number_past2;
+		number_past2 = number_past1;
+		number_past1 = number;
+	}
+
+	printf("%lli", number);
+
+	return 0;
+}
---
+++ home_work3/6.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+#define YEAR 1300
+#define LENGHT_YEAR 365
+
+#define MONTH 1
+#define HALF_YEAR 6
+#define LENGHT_MONTH1 31
+#define LENGHT_MONTH2 30
+
+#define DAY 1
+
+int main(void)
+{
+	int input_day = 0;
+	scanf("%i", &input_day);
+
+	int buffer = input_day;
+
+	/*calculate year*/
+	int year = YEAR + buffer / LENGHT_YEAR;
+	buffer %= LENGHT_YEAR;
+
+	/*calculate month*/
+	int month = MONTH;
+	if (input_day < HALF_YEAR * LENGHT_MONTH1)
+	{
+		month += buffer / LENGHT_MONTH1;
+		buffer %= LENGHT_MONTH1;
+	}
+	else
+	{
+		buffer -= HALF_YEAR * LENGHT_MONTH1;
+		month = HALF_YEAR + buffer / LENGHT_MONTH2;
+		buffer %= LENGHT_MONTH2;
+	}
+
+	/*calculate day*/
+	int day = DAY + buffer;
+
+	/*print ans*/
+	printf("%i.%i.%i", year, month, day);
+
+	return 0;
+}
