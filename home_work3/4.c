649a799eb9a361b4e24d15c7151980fb9b62a2c3
mvajhi <mvajhimv@gmail.com>
2022-11-13 21:32:52 +0330

    rework hw3/4

--- home_work3/4.c
+++ home_work3/4.c
@@ -1,14 +1,29 @@
 #include<stdio.h>
 #include<math.h>
 
+#define True 1
+#define False 0
+
 int main(void)
 {
 	int input = 0;
 	scanf("%i", &input);
 
-	for (int i = 2; i < (float)sqrt(input); i++)
+	for (int i = 2; i < input; i++)
 	{
-		if (input % i == 0)
+		float sqrt_i = sqrt(i);
+		int is_prime = True;
+		
+		for (int j = 2; j < sqrt(i); j++)
+		{
+			if (i % j == 0)
+			{
+				is_prime = False;
+				break;
+			}
+		}
+
+		if (is_prime)
 		{
 			printf("%i ", i);
 		}
