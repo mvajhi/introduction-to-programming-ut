f6607adc6869e42a1ad77bdefa07b71f450d5076
mvajhi <mvajhimv@gmail.com>
2022-11-17 23:22:31 +0330

    remove array CA1/3.c

--- CA1/3.c
+++ CA1/3.c
@@ -6,12 +6,14 @@
 int main(void)
 {
 	/***get input and check that***/
-	float coeffcient[3] = {0};
+	float coeffcient1 = 0;
+	float coeffcient2 = 0;
+	float coeffcient3 = 0;
 
-	for (int i = 2; i >= 0; i--)
-	{
-		scanf("%f", &coeffcient[i]);
-	}
+	scanf("%f", &coeffcient3);
+	scanf("%f", &coeffcient2);
+	scanf("%f", &coeffcient1);
+
 	double approximation = 0;
 
 	scanf("%lf", &approximation);
@@ -24,12 +26,18 @@ int main(void)
 
 	/***find max and save max ^ 2 ***/
 	float max = 1;
-	for (int i = 0; i < 3; i++)
+
+	if (coeffcient1 > max)
 	{
-		if (coeffcient[i] > max)
-		{
-			max = coeffcient[i];
-		}
+		max = coeffcient1;
+	}
+	else if (coeffcient2 > max)
+	{
+		max = coeffcient2;
+	}
+	else if (coeffcient3 > max)
+	{
+		max = coeffcient3;
 	}
 
 	float pow_max_coeffcient = max * max;
@@ -37,7 +45,7 @@ int main(void)
 	/***find root(s) and print that***/
 	int counter = 0;
 	double test_number = -pow_max_coeffcient;
-	double function_value = (test_number * test_number * test_number) + coeffcient[2] * (test_number * test_number) + coeffcient[1] * test_number + coeffcient[0];
+	double function_value = (test_number * test_number * test_number) + coeffcient3 * (test_number * test_number) + coeffcient2 * test_number + coeffcient1;
 	double pre_function_value = function_value;
 
 	while (test_number < pow_max_coeffcient)
@@ -45,7 +53,7 @@ int main(void)
 
 		/*calculat 3rd degree function*/
 		test_number += approximation;
-		function_value = (test_number * test_number * test_number) + coeffcient[2] * (test_number * test_number) + coeffcient[1] * test_number + coeffcient[0];
+		function_value = (test_number * test_number * test_number) + coeffcient3 * (test_number * test_number) + coeffcient2 * test_number + coeffcient1;
 
 		/*Bolzano's theorem*/
 		if (function_value * pre_function_value <= 0)
