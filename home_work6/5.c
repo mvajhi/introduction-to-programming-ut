15cc1c1ad027083b1b076845909f225a644d2a6e
mvajhi <mvajhimv@gmail.com>
2022-12-21 17:27:35 +0330

    HW6/5.c write sorting and print name func

--- home_work6/5.c
+++ home_work6/5.c
@@ -14,39 +14,81 @@ typedef struct Student
 }
 Student;
 
+void sorting(Student *unsorted, Student *sorted);
+void print_name(Student *input);
+
 int main(void)
 {
 	int increase_len = 0;
 	int counter = 0;
-	scanf("%i", increase_len);
+	scanf("%i", &increase_len);
 
-	Student *arr = NULL;
+	Student *input = NULL;
 
 	while (True)
 	{
-		if (counter % increase_len == 0)
-		{
-			int len = sizeof(arr);
-			arr = (Student) realloc(arr, len + increase_len);
-		}
-		
 		char name[NAME_LEN];
 		int sid;
 		float gpa;
 
 		scanf("%s %i %f", name, &sid, &gpa);
 
-		if (!strcmp(name, "END"))
+		if (sid == 0)
 		{
 			break;
 		}
 
-		strcmp(arr[counter].name, name);
-		arr[counter].sid = sid;
-		arr[counter].gpa = gpa;
+		if (counter % increase_len == 0)
+		{
+			int len = sizeof(input);
+			input = (Student*) realloc(input, len + increase_len * sizeof(Student));
+		}
+			printf("%i\n", sizeof(input) / sizeof(Student));
+
+		strcmp(input[counter].name, name);
+		input[counter].sid = sid;
+		input[counter].gpa = gpa;
 
 		counter++;
 	}
 
+	Student *sorted = (Student*) malloc(sizeof(input));
+
+	sorting(input, sorted);
+
+	print_name(sorted);
+
 	return 0;
+}
+
+void sorting(Student *unsorted, Student *sorted)
+{
+	memcpy(sorted, unsorted, sizeof(unsorted));
+	int len = sizeof(unsorted) / sizeof(Student);
+	for (int i = 0; i < len; i++)
+	{
+		Student highest_gpa_stu = sorted[i];
+		int j;
+		for (j = i; j < len; j++)
+		{
+			if (highest_gpa_stu.gpa < sorted[j].gpa)
+			{
+				highest_gpa_stu = sorted[j];
+			}
+		}
+		
+		sorted[j] = unsorted[i];
+		sorted[i] = highest_gpa_stu;
+	}
+}
+
+
+void print_name(Student *input)
+{
+	int len = sizeof(input) / sizeof(Student);
+
+	for (int i = 0; i < len; i++)
+	{
+		printf("%i. %s\n", i + 1, input[i].name);
+	}
 }
